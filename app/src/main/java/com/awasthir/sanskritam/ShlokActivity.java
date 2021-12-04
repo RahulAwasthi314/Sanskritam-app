32328d7c963f181c9b831be50aada4d628a6c61f
Rahul Awasthi <awasthir@pm.me>
2021-12-05 00:27:29 +0530

    Added SholkActivity

---
+++ app/src/main/java/com/awasthir/sanskritam/ShlokActivity.java
@@ -0,0 +1,14 @@
+package com.awasthir.sanskritam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShlokActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shlok);
+    }
+}
